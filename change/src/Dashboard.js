332a0d5875f0962369519e2c03e7359f904b6602
jmatray <jmatray@uw.edu>
2018-05-15 17:15:05 -0700

    Add placeholder components and navbar

---
+++ change/src/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+import firebase from 'firebase';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <p> Dashboard </p>
+        )
+    }
+}
+export default Dashboard;
