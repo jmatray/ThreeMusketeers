4094fa10e1cdbca008833ba41d621d12b6dcb584
Christine Smet <msmet@uw.edu>
2018-06-02 16:53:39 -0700

    add validation for expenses

--- change/src/components/Expenses.js
+++ change/src/components/Expenses.js
@@ -8,20 +8,8 @@ class Expenses extends Component {
     constructor(props, context) {
         super(props, context);
 
-        this.handleCategoryOneChange = this.handleCategoryOneChange.bind(this);
-        this.handleCategoryOneValueChange = this.handleCategoryOneValueChange.bind(this);
-        this.handleC1CheckboxChange = this.handleC1CheckboxChange.bind(this);
-        this.handleC1IsItChecked = this.handleC1IsItChecked.bind(this)
-
-        this.handleCategoryTwoChange = this.handleCategoryTwoChange.bind(this);
-        this.handleCategoryTwoValueChange = this.handleCategoryTwoValueChange.bind(this);
-        this.handleC2CheckboxChange = this.handleC2CheckboxChange.bind(this);
-        this.handleC2IsItChecked = this.handleC2IsItChecked.bind(this)
-
-        this.handleCategoryThreeChange = this.handleCategoryThreeChange.bind(this);
-        this.handleCategoryThreeValueChange = this.handleCategoryThreeValueChange.bind(this);
-        this.handleC3CheckboxChange = this.handleC3CheckboxChange.bind(this);
-        this.handleC3IsItChecked = this.handleC3IsItChecked.bind(this);
+        this.getValidationStateForExpenseName = this.getValidationStateForExpenseName.bind(this);
+        this.getValidationStateForExpenseValue = this.getValidationStateForExpenseValue.bind(this);
 
         this.onClick = this.onClick.bind(this)
 
@@ -40,15 +28,6 @@ class Expenses extends Component {
             },
             income: income,
             savings: savings,
-            categoryOne: 'Category Name',
-            categoryOneValue: 'Enter monetary value here',
-            categoryOneChecked: false,
-            categoryTwo: 'Category Name',
-            categoryTwoValue: 'Enter monetary value here',
-            categoryTwoChecked: false,
-            categoryThree: 'Category Name',
-            categoryThreeValue: 'Enter monetary value here',
-            categoryThreeChecked: false,
             basicExpenses: ['input-0'],
             /* get user's estimated monthly income from GetUserData */
             /* get user's estimated monthly savings from GetUserData */
@@ -58,12 +37,14 @@ class Expenses extends Component {
         };
     }
 
-    getValidationStateForCategoryOne() {
-        var input = this.state.categoryOne;
-        if (this.state.categoryOne === 'Category Name') {
-            return this.state.categoryOne;
+    
+
+    getValidationStateForExpenseName() {
+        var input = this.state.expenses.name; // msArr["objectId"] !== undefined
+        if (this.state.expenses.name === undefined) {
+            return this.state.expenses.name;
         }
-        var regex = /[^a-zA-Z]+/g; // this could be wrong
+        var regex = /[^a-zA-Z]+/g; // this could be wrong 
         if (regex.test(input)) {
             return 'success';
         } else {
@@ -71,14 +52,10 @@ class Expenses extends Component {
         }
     }
 
-    handleCategoryOneChange(e) {
-        this.setState({ categoryOne: e.target.value });
-    }
-
-    getValidationStateForCategoryOneValue() {
-        var input = this.state.categoryOneValue;
-        if (this.state.categoryOneValue === 'Enter monetary value here') {
-            return this.state.categoryOneValue;
+    getValidationStateForExpenseValue() {
+        var input = this.state.expenses["value"]; // msArr["objectId"] !== undefined
+        if (this.state.expenses["value"] === undefined) {
+            return this.state.expenses["value"];
         }
         var regex = /^\d+(?:\.\d{1,2})?$/; // this could be wrong
         if (regex.test(input)) {
@@ -88,125 +65,6 @@ class Expenses extends Component {
         }
     }
 
-    handleCategoryOneValueChange(e) {
-        this.setState({ categoryOneValue: e.target.value });
-    }
-
-    handleCategoryOneChecked(evt) {
-        this.setState({ checkboxChecked: evt.target.checked });
-    }
-
-    handleCategoryOneIsItChecked() {
-        console.log(this.state.checkboxChecked ? 'Yes' : 'No');
-    }
-
-    handleC1CheckboxChange(evt) {
-        this.setState({ categoryOneChecked: evt.target.checked });
-    }
-
-    handleC1IsItChecked() {
-        console.log(this.state.categoryOneChecked ? 'Yes' : 'No');
-    }
-
-    getValidationStateForCategoryTwo() {
-        var input = this.state.categoryTwo;
-        if (this.state.categoryTwo === 'Category Name') {
-            return this.state.categoryTwo;
-        }
-        var regex = /[^a-zA-Z]+/g; // this could be wrong
-        if (regex.test(input)) {
-            return 'success';
-        } else {
-            return 'error';
-        }
-    }
-
-    handleCategoryTwoChange(e) {
-        this.setState({ categoryTwo: e.target.value });
-    }
-
-    getValidationStateForCategoryTwoValue() {
-        var input = this.state.categoryTwoValue;
-        if (this.state.categoryTwoValue === 'Enter monetary value here') {
-            return this.state.categoryOneValue;
-        }
-        var regex = /^\d+(?:\.\d{1,2})?$/; // this could be wrong
-        if (regex.test(input)) {
-            return 'success';
-        } else {
-            return 'error';
-        }
-    }
-
-    handleCategoryTwoValueChange(e) {
-        this.setState({ categoryTwoValue: e.target.value });
-    }
-
-    handleCategoryTwoChecked(evt) {
-        this.setState({ checkboxChecked: evt.target.checked });
-    }
-
-    handleCategoryTwoIsItChecked() {
-        console.log(this.state.checkboxChecked ? 'Yes' : 'No');
-    }
-
-    handleC2CheckboxChange(evt) {
-        this.setState({ categoryTwoChecked: evt.target.checked });
-    }
-
-    handleC2IsItChecked() {
-        console.log(this.state.categoryTwoChecked ? 'Yes' : 'No');
-    }
-
-    getValidationStateForCategoryThree() {
-        var input = this.state.categoryThree;
-        if (this.state.categoryThree === 'Category Name') {
-            return this.state.categoryThree;
-        }
-        var regex = /[^a-zA-Z]+/g; // this could be wrong
-        if (regex.test(input)) {
-            return 'success';
-        } else {
-            return 'error';
-        }
-    }
-
-    handleCategoryThreeChange(e) {
-        this.setState({ categoryThree: e.target.value });
-    }
-
-    getValidationStateForCategoryThreeValue() {
-        var input = this.state.categoryThreeValue;
-        if (this.state.categoryThreeValue === 'Enter monetary value here') {
-            return this.state.categoryThreeValue;
-        }
-        var regex = /^\d+(?:\.\d{1,2})?$/; // this could be wrong
-        if (regex.test(input)) {
-            return 'success';
-        } else {
-            return 'error';
-        }
-    }
-
-    handleCategoryThreeValueChange(e) {
-        this.setState({ categoryThreeValue: e.target.value });
-    }
-
-    handleCategoryThreeChecked(evt) {
-        this.setState({ checkboxChecked: evt.target.checked });
-    }
-
-    handleCategoryThreeIsItChecked() {
-        console.log(this.state.checkboxChecked ? 'Yes' : 'No');
-    }
-
-    handleC3CheckboxChange(evt) {
-        this.setState({ categoryThreeChecked: evt.target.checked });
-    }
-
-    handleC3IsItChecked() {
-        console.log(this.state.categoryThreeChecked ? 'Yes' : 'No');
-    }
 
     onSubmitClick(e) {
         console.log("in here!");
@@ -279,13 +137,13 @@ class Expenses extends Component {
                                 <div className="Expense">
                                     <input
                                         type="text"
-                                        placeholder={`Expense #${idx + 1} name`}
+                                        placeholder={`Expense ${idx + 1} name`}
                                         value={expense.name}
                                         onChange={this.handleExpenseNameChange(idx)}
                                     />
                                     <input
                                         type="text"
-                                        placeholder={`Expense #${idx + 1} Amount`}
+                                        placeholder={`Expense ${idx + 1} Amount`}
                                         value={expense.value}
                                         onChange={this.handleExpenseValueChange(idx)}
                                     />
@@ -293,20 +151,19 @@ class Expenses extends Component {
                                 </div>
                             ))}
                             <button type="button" onClick={this.handleAddExpense} className="small">Add Expense</button>
+                            <Col xs={12} className="save-cancel-bar">
+                        <Button>Cancel</Button>
+
+                        <Button disabled={
+                            this.getValidationStateForExpenseName() !== "success" ||
+                            this.getValidationStateForExpenseValue() !== "success"}
+                            onClick={this.formatForSubmit}>Save</Button>
+                    </Col>
                         </form>
                     </Col>
                 </Row>
                 <Row>
-                    <Col xs={12} className="save-cancel-bar">
-                        <Button>Cancel</Button>
-
-                        <Button disabled={
-                            this.getValidationStateForCategoryOne() !== "success" ||
-                            this.getValidationStateForCategoryOneValue() !== "success" ||
-                            this.getValidationStateForCategoryTwo() !== "success" ||
-                            this.getValidationStateForCategoryTwoValue() !== "success"}
-                            onClick={this.formatForSubmit}>Save</Button>
-                    </Col>
+                    
                 </Row>
             </div>
         );
